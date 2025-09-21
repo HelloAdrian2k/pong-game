cf637d4f2a23b06de966e1a8e1e532d0d70e04a8
HelloAdrian2k <mz.adriaan@gmail.com>
2025-09-21 11:37:42 +0200

    feat(main): create right paddle

--- main.py
+++ main.py
@@ -5,4 +5,11 @@ screen.setup(width=800, height=600)
 screen.bgcolor('black')
 screen.title('Pong')
 
+paddle = Turtle()
+paddle.shape('square')
+paddle.color('white')
+paddle.shapesize(stretch_wid=5, stretch_len=1)
+paddle.penup()
+paddle.goto(350, 0)
+
 screen.mainloop()
