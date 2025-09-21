5372c6eccd60115b87c0cef18a86f62d44cbfa69
HelloAdrian2k <mz.adriaan@gmail.com>
2025-09-21 11:41:22 +0200

    feat(main): create screen event listeners and functions to move paddle

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 from turtle import Turtle, Screen
 
+def go_up():
+  new_ycor = paddle.ycor() + 20
+  paddle.goto(paddle.xcor(), new_ycor)
+
+def go_down():
+  new_ycor = paddle.ycor() - 20
+  paddle.goto(paddle.xcor(), new_ycor)
+
 screen = Screen()
 screen.setup(width=800, height=600)
 screen.bgcolor('black')
@@ -11,5 +19,9 @@ paddle.color('white')
 paddle.shapesize(stretch_wid=5, stretch_len=1)
 paddle.penup()
 paddle.goto(350, 0)
+
+screen.listen()
+screen.onkey(go_up, 'Up')
+screen.onkey(go_down, 'Down')
 
 screen.mainloop()
